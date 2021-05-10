cf5847abae0a44ea128e28635e1d72ffe5cfb5c7
Raúl Duhalde <RaulDuha@MacBook-Air-de-AirMac.local>
2021-05-09 22:30:26 -0400

    second commit

--- lab_4.js
+++ lab_4.js
@@ -1,1 +1,37 @@
 var prompt = require('prompt-sync')();
+var counter = 0;
+
+function view(counter) {
+    return 'Count = ' + counter + '\n' +
+        '\n' +
+        '(+)  (-) \n' +
+        '\n' +
+        '\n' +
+        '(q) for quit' +
+        '\n'
+
+}
+
+function update(msg, counter) {
+    if (msg === '+') {
+        return counter + 1;
+    } else if (msg === '-') {
+        return counter - 1;
+    } else {
+        return counter;
+    }
+
+}
+
+function app(counter) {
+    while (true) {
+        const currentView = view(counter)
+        console.clear()
+        console.log(view(counter))
+        let des = prompt('What would you do? : ')
+        const msg = des
+        counter = update(msg, counter)
+    }
+
+}
+app(0)
